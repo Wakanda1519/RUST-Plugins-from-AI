fb77ea516328b7e7b00990ac0c4a998d09031b11
wakanda <dvornikov-2005@inbox.ru>
2025-09-14 15:03:28 +0300

    Add files via upload
    FIX

--- WLockManager.cs
+++ WLockManager.cs
@@ -76,8 +76,7 @@ namespace Oxide.Plugins
             
             // Проверяем, является ли объект дверью или ящиком
             bool isDoor = entity is Door;
-            bool isBox = (entity.PrefabName.Contains("box.wooden") || entity.PrefabName.Contains("large.wood") || entity.PrefabName.Contains("coffin.storage") || entity is StorageContainer) 
-                         && !entity.PrefabName.Contains("workbench") && !entity.PrefabName.Contains("autoturret");
+            bool isBox = entity.PrefabName.Contains("box.wooden") || entity.PrefabName.Contains("large.wood");
             if (!isDoor && !isBox) return;
             if (entity.IsLocked()) return;
             
